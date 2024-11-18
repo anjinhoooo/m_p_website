60ee8686db421d99e274af465681f7821a4af24e
anjinhoooo <pedroanjinhosilvafiverr@gmail.com>
2024-11-18 16:36:11 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/m_p_website/',  // Replace 'repository-name' with your actual repo name
+  base: '/',  // Replace 'repository-name' with your actual repo name
 })
